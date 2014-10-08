f41c9fcf36094b0e35a6639a734496b8e9285206
Oriol Pérez <oriolper@gmail.com>
2014-10-08 16:40:33 +0200

    Added User.java

---
+++ src/User.java
@@ -0,0 +1,46 @@
+public class User {
+	private int id; 
+	private String name;
+	private String surname;
+	private int age;
+
+	public User(int id, String name, String surname, int age){
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+	}
+
+	public String getName() {
+	    return this.name;
+	}
+
+	public String getSurname() {
+	    return this.surname;
+	}
+
+	public int getAge() {
+	    return this.age;
+	}
+
+	public void setName(String name) {
+	    this.name = name;
+	}
+
+	public void setSurname(String surname) {
+	    this.surname = surname;
+	}
+
+	public void setAge(int age) {
+	    this.age = age;
+	}
+	public int getId() {
+	    return this.id;
+	}
+
+	public void setId(int id) {
+	    this.id = id;
+	}
+
+
+}
