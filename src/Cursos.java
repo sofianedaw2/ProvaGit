f7a46f9092c84cf7baff6a647c33608815655ad9
Sofiane Mabtoul <sufianmab@gmail.com>
2014-10-29 13:03:37 +0100

    Create Cursos.java

---
+++ src/Cursos.java
@@ -0,0 +1,27 @@
+public class Cursos {
+	private int id; 
+	private String name;
+
+	public User(int id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+	    return this.id;
+	}
+	
+	public String getName() {
+	    return this.name;
+	}
+
+	public void setName(String name) {
+	    this.name = name;
+	}
+	
+	public void setId(int id) {
+	    this.id = id;
+	}
+
+
+}
